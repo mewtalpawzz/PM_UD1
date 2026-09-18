3a05d851e7e8e342e2f7315e74c204e89cdba6b5
mewtalpawz <mewtalpawz@gmail.com>
2026-09-18 16:38:01 +0200

    Ejercicios que vas haciendo oleeee, haces algo en serio

--- src/practicas/Ejercicio3.kt
+++ src/practicas/Ejercicio3.kt
@@ -1,11 +1,35 @@
 package practicas
 
 fun main(){
-    val notas = arrayOf<Double>()
-    println("Digame la primera nota")
-    val nota1 = readln()
+    val notas = DoubleArray(5)
 
-
+    for (i in 0 until notas.size){
+        println("Digame el ${i+1} numero")
+        val entrada = readln()
+        val nota = entrada.toDoubleOrNull() ?: 0.0
+        notas[i] = nota
+    }
     // 6.75, 8.40, 9.25, 6.05, 7.30, SUPONGO
+    var suma = 0.0
 
+    for (i in notas){
+        suma += i
+    }
+
+    val promedio = suma/notas.size
+
+    var notaMax = 0.0
+
+    for (i in notas){
+        if (i > notaMax){
+            notaMax = i
+        }
+    }
+
+    println("""
+        Las notas introducidas son ${notas.joinToString(", ")} 
+        La suma es: $suma  
+        El promedio de todas las notas es $promedio 
+        La nota más alta $notaMax
+    """.trimIndent())
 }
